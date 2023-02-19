9ebdabc7dbca0474dfc38d9e21749cd775574277
rnjstjdgh <gshgsh0831@daum.net>
2023-02-19 16:24:57 +0900

    디비 설정까지 해서 기본 세팅 끝난 상태

--- src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
@@ -12,49 +12,40 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RequiredArgsConstructor
 @Configuration
-public class HelloJobConfiguration {
+@RequiredArgsConstructor
+public class JobInstanceConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                .start(helloStep1())
-                .next(helloStep2())
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
                 .build();
     }
 
     @Bean
-    public Step helloStep1() {
-        return stepBuilderFactory.get("helloStep1")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("================================================");
-                        System.out.println("Hello Spring batch1");
-                        System.out.println("================================================");
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
-
     }
 
     @Bean
-    public Step helloStep2() {
-        return stepBuilderFactory.get("helloStep2")
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("================================================");
-                        System.out.println("Hello Spring batch2");
-                        System.out.println("================================================");
-                        return RepeatStatus.FINISHED;                    }
+                        return RepeatStatus.FINISHED;
+                    }
                 }).build();
     }
-
-
-
 }
