76670a8055e1b31e20ab7b94d6ca74315bf8fbc0
rnjstjdgh <gshgsh0831@daum.net>
2023-03-05 15:36:58 +0900

    incrementer

--- src/main/java/io/springbatch/springbatchlecture/job2/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/job2/JobConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -27,8 +28,9 @@ public class JobConfiguration {
         return jobBuilderFactory.get("job2")
                 .start(step1())
                 .next(step2())
-                .validator(new DefaultJobParametersValidator(requiredKeys, optionalKeys))
-//                .preventRestart()
+//                .validator(new DefaultJobParametersValidator(requiredKeys, optionalKeys))
+                .preventRestart()
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
@@ -50,7 +52,8 @@ public class JobConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("step2");
-                        return RepeatStatus.FINISHED;
+                        throw new RuntimeException("haha");
+//                        return RepeatStatus.FINISHED;
                     }
                 }).build();
     }
